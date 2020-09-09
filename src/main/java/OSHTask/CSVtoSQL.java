7b1981e2ffaec6776d3b99f86994ee9d501cf58a
palutaartur <palutaartur@gmail.com>
2020-09-09 15:14:52 +0300

    Added code to check the results on real database just by replace :memory: with database name without bothering to delete manually the database after first run.

--- src/main/java/OSHTask/CSVtoSQL.java
+++ src/main/java/OSHTask/CSVtoSQL.java
@@ -1,14 +1,13 @@
 package OSHTask;
 
-import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 
 public class CSVtoSQL implements CSVtoSQLInterface {
     private static int result;
-    private String url = "jdbc:sqlite::memory:";
-    // "jdbc:sqlite:parseCSV.db";
-    // use this url to check the records in real database.
-    //for the second run to succeed, delete the database parseCSV.db in the root folder of the project
+    public static final String TABLE_PARSE_CSV = ":memory:";
+    //"parseCSV.db"; // use this filename (or any name) to check the records in a real database.
+    //the database will be created in the root directory of the project // "jdbc:sqlite:parseCSV.db";
+    private String url = "jdbc:sqlite:" + TABLE_PARSE_CSV;
     public int getResult(){
         return result;
     }
@@ -19,8 +18,11 @@ public class CSVtoSQL implements CSVtoSQLInterface {
     private static String sqlValues = ""; //designed to hold n times "?" (number of columns) for sql statement insert into
     private String nameOfFields = ""; // designed to hold the names of columns, extracted from first header line
     //The method to get the number of columns for sqlValues ("?, ?, ?, ... n times") used in insert into table...
+
+
     Connection conn = DriverManager.getConnection(url);  //"jdbc:sqlite:D:\\Temp\\testjava.db"
     PreparedStatement pSt;
+
     public void setFields(String[] lineOne) throws SQLException {
         StringBuilder sb1 = new StringBuilder(sqlValues);
         for (String s : lineOne) {
@@ -31,15 +33,17 @@ public class CSVtoSQL implements CSVtoSQLInterface {
         pSt = conn.prepareStatement("insert into results values(" + sqlValues + ")");
          }
     //Creating the table, extracting column names from first line
+
     public void createTable(String[] lineOne) throws SQLException {
         Statement st = conn.createStatement();
+        st.execute("DROP TABLE IF EXISTS results");
         StringBuilder sb2 = new StringBuilder(nameOfFields);
         for (String s : lineOne) {
             sb2.append(s + " TEXT, ");
         }
         sb2.delete(sb2.length()-2, sb2.length());
         nameOfFields = sb2.toString();
-        st.execute("CREATE TABLE results (" + nameOfFields + ")");
+        st.execute("CREATE TABLE IF NOT EXISTS results (" + nameOfFields + ")");
         st.close();
     }
     //Insert values into table
--- src/main/java/OSHTask/Main.java
+++ src/main/java/OSHTask/Main.java
@@ -11,24 +11,33 @@ import java.util.Date;
 public class Main {
     private static final String RESOURCES = "src/main/resources/";
     private static String splitRegex = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-    private static Path csvPath = Paths.get(RESOURCES + "Interview-task-data-osh.csv");
+    private static Path csvPath;
     private static String badDataFileName = new SimpleDateFormat("'bad-data-'yyyy-MM-dd-HH-mm'.csv'").format(new Date());
     private static Path pathErr = Paths.get(RESOURCES + badDataFileName);
     private static Path pathLog = Paths.get(RESOURCES + "logFile.log");
 
     public Main() throws SQLException {
     }
-
+    private static void setFilename(String[] args){
+        String filename;
+        if (args.length == 0){
+            filename = "Interview-task-data-osh.csv";
+        }
+        else{
+            filename = args[0].toString();
+        }
+        csvPath = Paths.get(RESOURCES + filename);
+    }
     public static void main(String[] args) throws IOException, SQLException {
-            ParseCSV parseCSV = new ParseCSV(csvPath, pathErr, splitRegex);
+        setFilename(args);
+        ParseCSV parseCSV = new ParseCSV(csvPath, pathErr, splitRegex);
             ParseErrorsToCsv parseErr = new ParseErrorsToCsv(pathErr);
             CSVtoSQLInterface csVtoSQLite = new CSVtoSQL();
             parseCSV.parseCSV(parseErr, csVtoSQLite);
-            LogFile logFile = new LogFile();
             String log = "Successfully added "+ csVtoSQLite.getResult() + " records, \nFailed records: "
                     +parseErr.getFailedRecords() + "\nTotal number of records: " + (csVtoSQLite.getResult() +parseErr.getFailedRecords()) ;
             System.out.println(log);
-            logFile.log(pathLog, log);
+            LogFile.log(pathLog, log);
 
     }
 }
